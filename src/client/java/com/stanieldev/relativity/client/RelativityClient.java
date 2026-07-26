a3e6bef434f7253ab1c1c0830fd04dc786b15af3
Stanley Goodwin <67250149+TheAlaskanPhysicist@users.noreply.github.com>
2026-07-26 11:41:06 -0400

    Basic F4 + T trajectory toggle

--- src/client/java/com/stanieldev/relativity/client/RelativityClient.java
+++ src/client/java/com/stanieldev/relativity/client/RelativityClient.java
@@ -1,10 +1,14 @@
 package com.stanieldev.relativity.client;
 
+import com.stanieldev.relativity.client.debug.DebugState;
 import com.stanieldev.relativity.history.HistoryManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.stanieldev.relativity.client.render.PositionTrajectoryRenderer;
@@ -12,16 +16,47 @@ import com.stanieldev.relativity.client.render.PositionTrajectoryRenderer;
 public class RelativityClient implements ClientModInitializer {
 	public static final String MOD_ID = "relativity";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static KeyMapping showEntityTrajectoryKey;
+
+	private static boolean isF4Down() {
+		long window = Minecraft.getInstance().getWindow().getWindow();
+		return GLFW.glfwGetKey(window, GLFW.GLFW_KEY_F4) == GLFW.GLFW_PRESS;
+	}
+
 
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		WorldRenderEvents.AFTER_ENTITIES.register(context -> {
-			PositionTrajectoryRenderer.render(
-					context.matrixStack(),
-					context.consumers()
-			);
+
+		// Debug Toggling Keybinds
+		showEntityTrajectoryKey = new KeyMapping(
+				"key.relativity.toggle_show_entity_trajectory",
+				GLFW.GLFW_KEY_T,
+				"category.relativity"
+		);
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (showEntityTrajectoryKey.consumeClick()) {
+				if (isF4Down()) {
+					DebugState.toggleShowEntityTrajectory();
+					System.out.println("Trajectory rendering: " + DebugState.showEntityTrajectory);
+                    assert Minecraft.getInstance().player != null;
+                    Minecraft.getInstance().player.displayClientMessage(
+						Component.literal("Trajectory: " + (DebugState.showEntityTrajectory ? "ON" : "OFF")), true
+					);
+				}
+			}
 		});
+
+		// Debug Rendering
+		WorldRenderEvents.AFTER_ENTITIES.register(context -> {
+			if (DebugState.showEntityTrajectory) {
+				PositionTrajectoryRenderer.render(
+						context.matrixStack(),
+						context.consumers()
+				);
+			}
+		});
+
+		// History Tracking
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			Minecraft minecraft = Minecraft.getInstance();
 			if (minecraft.level == null) {
---
+++ src/client/java/com/stanieldev/relativity/client/debug/DebugState.java
@@ -0,0 +1,9 @@
+package com.stanieldev.relativity.client.debug;
+
+public class DebugState {
+    public static boolean showEntityTrajectory = false;
+
+    public static void toggleShowEntityTrajectory() {
+        showEntityTrajectory = !showEntityTrajectory;
+    }
+}
